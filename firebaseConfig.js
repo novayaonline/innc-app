430a8dddc4a68157dae1b12e4302ca6e7b9619f5
Felipe Maifredo <felipemaifredo@gmail.com>
2024-08-15 22:37:38 -0300

    att

---
+++ firebaseConfig.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app"
+import { getAuth } from "firebase/auth"
+import firebase from "firebase/compat/app"
+import "firebase/compat/firestore"
+import "firebase/compat/storage"
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+firebase.initializeApp(firebaseConfig)
+
+export const auth = getAuth(app)
+export const db = firebase.firestore()
+export const fs = firebase.storage()
